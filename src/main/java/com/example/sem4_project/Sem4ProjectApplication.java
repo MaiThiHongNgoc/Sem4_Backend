6fe62fc89b4fb74f60e4d7404edcd8ea198106c0
MaiThiHongNgoc <hongngoc200509@gmail.com>
2025-04-21 14:52:30 +0700

    oki

---
+++ src/main/java/com/example/sem4_project/Sem4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sem4_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem4ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sem4_project/Sem4ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem4_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
